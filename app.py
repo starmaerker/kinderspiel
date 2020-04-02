55fe9901b54590779fb605f6c5c368b1f48024e7
Andreas Gropp <starmaerker@googlemail.com>
2020-04-02 07:13:23 +0200

    add index.html

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import random_datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    first = f'{random_datetime.hours_one}:{random_datetime.minutes_one}'
+    second = f'{random_datetime.hours_two}:{random_datetime.minutes_two}'
+    delta = random_datetime.delta
+
+    return render_template('index.html', first=first, second=second, delta=delta)
 
 
 if __name__ == '__main__':
