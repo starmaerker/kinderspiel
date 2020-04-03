d1627af25d6c1f1232d3107bf7418b47cd6d952f
Andreas Gropp <starmaerker@googlemail.com>
2020-04-03 20:30:53 +0200

    add javascript check function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, make_response
 import random_datetime
 
 app = Flask(__name__)
@@ -7,8 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     first, second, delta = random_datetime.calc()
-    print(first)
-    print(type(first))
 
     return render_template('index.html', first=first, second=second, delta=delta)
 
