70691db0766aa10ad4fb2433aaf26ff1ae653e28
Andreas Gropp <starmaerker@googlemail.com>
2020-04-07 17:03:42 +0200

    create database_setup.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request, url_for, redirect, make_response
 import random_datetime
 
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    database_setup.get_db()
+
     first, second, delta = random_datetime.calc()
 
     return render_template('index.html', first=first, second=second, delta=delta)
@@ -13,3 +16,6 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+import database_setup
